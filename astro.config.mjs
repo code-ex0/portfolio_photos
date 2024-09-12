53460ec1df9131394697db663bdeba74f8ffa477
louis sasse <louis.sasse@ynov.com>
2024-09-12 18:58:33 +0200

    Add deployment workflow for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portfolio.louis-sasse.com',
   integrations: [react(),     
     tailwind({
       applyBaseStyles: false,
